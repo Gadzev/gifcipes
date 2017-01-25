62d91e2f60818c705bb9bf42ff3103eda8fe11e5
lignux <lignux@debian.example.com>
2017-01-25 15:55:14 +0100

    Project setup

---
+++ server/src/app.js
@@ -0,0 +1,31 @@
+// npm packages
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+// our packages
+import {logger} from '../util';
+
+// init app
+const app = express();
+
+// setup logging
+app.use(morgan('combined', {stream: logger.stream}));
+
+// add body parsing
+app.use(bodyParser.json()); // for parsing application json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
+// test method
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// catch all unhandled errors
+app.use((err, req, res) => {
+  logger.error(err.stack);
+  res.status(500).send(err);
+});
+
+// export app
+export default app;
