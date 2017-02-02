b11de3814d17efa9ceec243efc9681dae7b349e4
Gadzev <gadzevalexandar@openmailbox.org>
2017-02-03 00:26:03 +0100

    Use ES5 syntax for config, allow overriding config values with env variables

--- server/config.js
+++ server/config.js
@@ -1,11 +1,14 @@
-export const db = {
-    host: 'localhost',
-    port: 28015,
+exports.db = {
+    host: process.env.GIFCIPES_DB_URL || 'localhost',
+    port: process.env.GIFCIPES_DB_PORT || 28015,
     db: 'gifcipesdb',
 };
 
-export const auth = {
-  passwordSalt: '1a4xJQBlM0yS6g4syo1Dmqch',
-  sessionSecret: 'WLqydVMz4fE15jx1bqe9TuRT',
-  jwtSecret: 'lDcvkT4axMWkfx2lmpaeDJIe',
+exports.auth = {
+  passwordSalt: process.env.GIFCIPES_AUTH_PASSSALT ||
+      '1a4xJQBlM0yS6g4syo1Dmqch',
+  sessionSecret: process.env.GIFCIPES_AUTH_SESSECRET ||
+      'WLqydVMz4fE15jx1bqe9TuRT',
+  jwtSecret: process.env.GIFCIPES_AUTH_JWTSECRET ||
+      'lDcvkT4axMWkfx2lmpaeDJIe',
 };
