4527276f172bb177fad35c9cab1c3d8f23cf875b
Gadzev <gadzevalexandar@openmailbox.org>
2017-02-05 19:40:22 +0100

    Basic redux and router setup

---
+++ client/src/home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router';
+import {connect} from 'react-redux';
+
+import {helloWorldAction} from '../store';
+
+const mapStateProps = state => ({
+  world: state.helloWorld.world,
+});
+
+const mapDispatchProps = dispatch => ({
+  onClick: () => dispatch(helloWorldAction()),
+});
+
+const Home = ({onClick, world}) => (
+  <div className="jumbotron">
+    <h1>Hello {world}!</h1>
+    <button onClick={onClick}>Click me!</button>
+    <Link to="/other">other</Link>
+  </div>
+ );
+
+ export default connect(mapStateProps, mapDispatchProps)(Home);
