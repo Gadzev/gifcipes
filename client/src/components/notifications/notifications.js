69f00a60ef645d6c020d48e134052a4aef4b5964
Gadzev <gadzevalexandar@openmailbox.org>
2017-02-09 22:59:46 +0100

    Use ReactCSSTransitionGroup to animate notifications

--- client/src/components/notifications/notifications.js
+++ client/src/components/notifications/notifications.js
@@ -1,24 +1,31 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import Notification from './notification';
+// import transitions from './transitions.css';
 
 const mapStateToProps = state => ({
-    notifications: state.notifications,
+  notifications: state.notifications,
 });
 
 const Notifications = ({notifications}) => (
-    <div>
-        {
-            notifications.map(notification => (
-                <Notification key={notification.id} notification={notification} />
-            ))
-        }
-    </div>
+  <div>
+    <ReactCSSTransitionGroup
+      transitionName="transitions"
+      transitionEnterTimeout={700}
+      transitionLeaveTimeout={700}>
+      {
+        notifications.map(notification => (
+          <Notification key={notification.id} notification={notification} />
+        ))
+      }
+    </ReactCSSTransitionGroup>
+  </div>
 );
 
 Notifications.propTypes = {
-    notifications: PropTypes.array.isRequired,
+  notifications: PropTypes.array.isRequired,
 };
 
 export default connect(mapStateToProps)(Notifications);
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import {Provider} from 'react-redux';
 
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/components/notifications/transitions.css';
 
 // our packages
 import App from './app';
