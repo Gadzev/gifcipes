dbf1d99b4e0cb09ef5448d797ab19238c87eb746
Gadzev <gadzevalexandar@openmailbox.org>
2017-02-09 23:03:02 +0100

    Fix auth epic error

--- client/src/store/epics/auth.js
+++ client/src/store/epics/auth.js
@@ -1,48 +1,53 @@
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import * as ActionTypes from '../actionTypes';
 import * as Actions from '../actions';
-import { loginErrorToMessage, registerErrorToMessage } from '../../util';
+import {loginErrorToMessage, registerErrorToMessage} from '../../util';
 
 export const login = action$ => action$
-    .ofType(ActionTypes.DO_LOGIN)
-    .switchMap(({ payload }) => Observable
-        .ajax.post('http://localhost:8080/api/login', payload)
-        .map(res => res.response)
-        .map(response => ({
-            type: ActionTypes.LOGIN_SUCCESS,
-            payload: response,
-        }))
-        .concat(Observable.of(
-            Actions.addNotificationAction({ text: 'Login success', alertType: 'info' }),
-        ))
-        .catch(error => Observable.of({
-                type: ActionTypes.LOGIN_ERROR,
-                payload: {
-                    error,
-                },
-            },
-            Actions.addNotificationAction({ text: loginErrorToMessage(error), alertType: 'danger' }),
-        )),
-    );
+  .ofType(ActionTypes.DO_LOGIN)
+  .switchMap(({payload}) => Observable
+    .ajax.post('http://localhost:8080/api/login', payload)
+    .map(res => res.response)
+    .mergeMap(response => Observable.of(
+      {
+        type: ActionTypes.LOGIN_SUCCESS,
+        payload: response,
+      },
+      Actions.addNotificationAction(
+        {text: 'Login success', alertType: 'info'}),
+    ))
+    .catch(error => Observable.of(
+      {
+        type: ActionTypes.LOGIN_ERROR,
+        payload: {
+          error,
+        },
+      },
+      Actions.addNotificationAction({text: loginErrorToMessage(error), alertType: 'danger'}),
+    )),
+  );
 
 export const register = action$ => action$
-    .ofType(ActionTypes.DO_REGISTER)
-    .switchMap(({ payload }) => Observable
-        .ajax.post('http://localhost:8080/api/register', payload)
-        .map(res => res.response)
-        .map(response => ({
-            type: ActionTypes.REGISTER_SUCCESS,
-            payload: response,
-        }))
-        .concat(Observable.of(
-            Actions.addNotificationAction({ text: 'Register success', alertType: 'info' }),
-        ))
-        .catch(error => Observable.of({
-                type: ActionTypes.REGISTER_ERROR,
-                payload: {
-                    error,
-                },
-            },
-            Actions.addNotificationAction({ text: registerErrorToMessage(error), alertType: 'danger' }),
-        )),
-    );
+  .ofType(ActionTypes.DO_REGISTER)
+  .switchMap(({payload}) => Observable
+    .ajax.post('http://localhost:8080/api/register', payload)
+    .map(res => res.response)
+    .mergeMap(response => Observable.of(
+      {
+        type: ActionTypes.REGISTER_SUCCESS,
+        payload: response,
+      },
+      Actions.addNotificationAction(
+        {text: 'Register success', alertType: 'info'},
+      ),
+    ))
+    .catch(error => Observable.of(
+      {
+        type: ActionTypes.REGISTER_ERROR,
+        payload: {
+          error,
+        },
+      },
+      Actions.addNotificationAction({text: registerErrorToMessage(error), alertType: 'danger'}),
+    )),
+  );
